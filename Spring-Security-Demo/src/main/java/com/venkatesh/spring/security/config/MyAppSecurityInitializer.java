cd1bdb49034529d74812425418cf04e3f9388aac
venkateshpalagiri <venkateshpalagiri@gmail.com>
2023-12-05 13:48:14 -0500

    Enabled Web Security

---
+++ Spring-Security-Demo/src/main/java/com/venkatesh/spring/security/config/MyAppSecurityInitializer.java
@@ -0,0 +1,7 @@
+package com.venkatesh.spring.security.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MyAppSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
