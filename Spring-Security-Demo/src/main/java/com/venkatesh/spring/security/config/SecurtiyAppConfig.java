fe7f8396b47ea98e3c412ca57d8e986a1a92eb6c
venkateshpalagiri <venkateshpalagiri@gmail.com>
2023-12-06 11:58:01 -0500

    user created using InMemoryUserDetailsManager, PasswordEncoder

--- Spring-Security-Demo/src/main/java/com/venkatesh/spring/security/config/SecurtiyAppConfig.java
+++ Spring-Security-Demo/src/main/java/com/venkatesh/spring/security/config/SecurtiyAppConfig.java
@@ -2,18 +2,22 @@ package com.venkatesh.spring.security.config;
 
 import java.util.ArrayList;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity(debug=true)
 public class SecurtiyAppConfig {
-	
-	void setUpUser(){
+	@Bean
+	InMemoryUserDetailsManager setUpUser(){
 	
 //	GrantedAuthority role=new SimpleGrantedAuthority("admin");
 //	GrantedAuthority role1=new SimpleGrantedAuthority("user");
@@ -25,7 +29,14 @@ public class SecurtiyAppConfig {
 	authoritiesList.add(new SimpleGrantedAuthority("user"));
 	
 	UserDetails venkyUser=new User("venky","123",authoritiesList);
+	InMemoryUserDetailsManager inMemoryUserDetailsManager=new InMemoryUserDetailsManager();
+	inMemoryUserDetailsManager.createUser(venkyUser);
+	return inMemoryUserDetailsManager;
 	
+	}
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
 	}
 
 }
